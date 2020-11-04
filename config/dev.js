6e495dbe6cb8a6aaafc3a91d42115c4a287d5dd9
A F M Muzahidur Rahman <tareq.rahman06@gmail.com>
2020-11-04 02:21:06 +0200

    Travis config

--- config/dev.js
+++ config/dev.js
@@ -3,9 +3,6 @@
 =======
 >>>>>>> e2aa6d524b9a9e8938c6d2b5406812e9c11547ad
 module.exports = {
-  googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
   mongoURI: 'mongodb://dbNode:g46KeHA5wNwHfppn@cluster0-shard-00-00.k65vc.mongodb.net:27017,cluster0-shard-00-01.k65vc.mongodb.net:27017,cluster0-shard-00-02.k65vc.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-s5ppd8-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
