2b1df102112d14e926d061433432836d24f54938
A F M Muzahidur Rahman <tareq.rahman06@gmail.com>
2020-11-04 02:23:04 +0200

    Travis config

--- config/dev.js
+++ config/dev.js
@@ -3,7 +3,7 @@
 =======
 >>>>>>> e2aa6d524b9a9e8938c6d2b5406812e9c11547ad
 module.exports = {
-  mongoURI: 'mongodb://dbNode:g46KeHA5wNwHfppn@cluster0-shard-00-00.k65vc.mongodb.net:27017,cluster0-shard-00-01.k65vc.mongodb.net:27017,cluster0-shard-00-02.k65vc.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-s5ppd8-shard-0&authSource=admin&retryWrites=true&w=majority',
+  mongoURI: '',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };
